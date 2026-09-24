91e5e486c5f3270f9b52e16b90889345d099ea77
herwigwaschis-art <herwig.waschis@gmail.com>
2026-09-24 21:31:11 +0200

    Refresh cache for MILO workflow upgrade

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="milo-v28";
+const CACHE="milo-v29";
 const STATIC=["./","./index.html","./manifest.webmanifest","./milo-icon.svg","./milo-wordmark.svg"];
 
 self.addEventListener("install",event=>{
