08c8abae7355b3da45e065d51c768b9dbd5ca7a3
herwigwaschis-art <herwig.waschis@gmail.com>
2026-09-24 21:05:11 +0200

    Refresh cache for simplified class actions

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="milo-v26";
+const CACHE="milo-v27";
 const STATIC=["./","./index.html","./manifest.webmanifest","./milo-icon.svg","./milo-wordmark.svg"];
 
 self.addEventListener("install",event=>{
