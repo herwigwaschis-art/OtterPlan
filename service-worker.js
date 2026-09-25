9f70c0b1b2a0fa0c998fa3390840974bd39a6950
herwigwaschis-art <herwig.waschis@gmail.com>
2026-09-25 06:23:13 +0200

    Refresh cache for cleaner MILO navigation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="milo-v31";
+const CACHE="milo-v32";
 const STATIC=["./","./index.html","./manifest.webmanifest","./milo-icon.svg","./milo-wordmark.svg"];
 
 self.addEventListener("install",event=>{
