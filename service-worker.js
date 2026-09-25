7618d9010a7d29ac7eb18ccaad33a269f14f3752
herwigwaschis-art <herwig.waschis@gmail.com>
2026-09-25 13:01:41 +0200

    Use MILO lettering for iPad and PWA icon

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="milo-v37";
+const CACHE="milo-v38";
 const STATIC=["./","./?menu=1","./index.html","./manifest.webmanifest","./milo-icon.svg","./milo-icon-180.png","./milo-icon-192.png","./milo-icon-512.png","./milo-wordmark.svg"];
 const LIBRARIES=new Set([
   "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2",
