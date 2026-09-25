0a759ac7bb776915c6df9446fc7801eb437317f9
herwigwaschis-art <herwig.waschis@gmail.com>
2026-09-25 06:41:17 +0200

    Refresh cache for quick-rating button style

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="milo-v33";
+const CACHE="milo-v34";
 const STATIC=["./","./index.html","./manifest.webmanifest","./milo-icon.svg","./milo-wordmark.svg"];
 
 self.addEventListener("install",event=>{
