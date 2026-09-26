2ea41883f90d1824eac1bd126068b2ff0eca0473
herwigwaschis-art <herwig.waschis@gmail.com>
2026-09-26 12:13:25 +0200

    Refresh cached Safari MILO icon

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="milo-v39";
-const STATIC=["./","./?menu=1","./index.html","./manifest.webmanifest","./milo-icon.svg","./milo-icon-180.png","./milo-icon-192.png","./milo-icon-512.png","./milo-wordmark.svg"];
+const CACHE="milo-v40";
+const STATIC=["./","./?menu=1","./index.html","./manifest.webmanifest","./milo-icon.svg","./milo-icon.svg?v=40","./milo-icon-180.png","./milo-icon-192.png","./milo-icon-512.png","./milo-wordmark.svg"];
 const LIBRARIES=new Set([
   "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2",
   "https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js",
